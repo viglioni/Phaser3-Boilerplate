974392545f60ef64e5647fb411cec25aa813cd36
Laura Viglioni <laura.viglioni@clickideia.com.br>
2019-01-18 14:23:59 -0200

    improvements

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,17 +1,21 @@
 var iwidth = window.innerWidth;
 var iheight = window.innerHeight;
 
+
+// Default size is 1200 x 675
+var max = 1200;
+
 export default class Constants {
     constructor(){
         this.width = iwidth;
         this.height = iheight;
-        this.scale = this.width/1200;
+        this.scale = this.width/max;
     }
 
     resize(){
-        this.width = Math.min(1200, Math.min(iwidth, Math.round(16*iheight/9)) );
+        this.width = Math.min(max, Math.min(iwidth, Math.round(16*iheight/9)) );
         this.height = Math.round(9*this.width/16);
-        this.scale = this.width/1200;
+        this.scale = this.width/max;
 
     }
 
